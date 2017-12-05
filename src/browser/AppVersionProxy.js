c273db8c34ca5ab9314c2e1066f8162c461b0a3a
quentin <quentin@tuto.com>
2017-12-05 10:41:40 +0100

    browser support

---
+++ src/browser/AppVersionProxy.js
@@ -0,0 +1,31 @@
+cordova.define("cordova-plugin-app-version.AppVersionProxy", function (require, exports, module) {
+  function getXmlConfig() {
+    var Connect = new XMLHttpRequest();
+    Connect.open("GET", "config.xml", false);
+    Connect.setRequestHeader("Content-Type", "text/xml");
+    Connect.send(null);
+    return Connect.responseXML;
+  }
+
+  module.exports = {
+    getAppName: function (success, error) {
+      success(getXmlConfig().getElementsByTagName("name")[0].firstChild.nodeValue);
+      error('ERROR getAppName');
+    },
+    getPackageName: function (success, error) {
+      success(getXmlConfig().getElementsByTagName("widget")[0].getAttribute("id"));
+      error('ERROR getPackageName');
+    },
+    getVersionNumber: function (success, error) {
+        success(getXmlConfig().getElementsByTagName("widget")[0].getAttribute("version"));
+      error('ERROR getVersionNumber');
+    },
+    getVersionCode: function (success, error) {
+      success(getXmlConfig().getElementsByTagName("widget")[0].getAttribute("version"));
+      error('ERROR getVersionCode');
+    }
+  };
+
+  require('cordova/exec/proxy').add('AppVersion', module.exports);
+
+});
